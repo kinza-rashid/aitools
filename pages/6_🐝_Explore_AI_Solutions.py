ee4fdc8778ab5e938adc51ea5f7171e8b1c3fe36
kinza-rashid <127532906+kinza-rashid@users.noreply.github.com>
2023-05-13 23:01:49 +0500

    Update 6_🐝_Explore_AI_Solutions.py

--- pages/6_🐝_Explore_AI_Solutions.py
+++ pages/6_🐝_Explore_AI_Solutions.py
@@ -55,8 +55,8 @@ else:
         col1.markdown(
             'GPT-4 is the most advanced large language model from Open AI. GPT-4 can solve difficult problems with greater accuracy, thanks to its broader general knowledge and problem solving abilities')
 
-        if col1.button("Explore GPT-4"):
-            webbrowser.open_new_tab('https://openai.com/product/gpt-4')
+        if col1.button("Explore GPT-4",'https://openai.com/product/gpt-4'):
+           st.write("")
 
         col2.image("images/gpt.jpeg")
 
