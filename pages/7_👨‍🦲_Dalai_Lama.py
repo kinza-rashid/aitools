071682084a47167a4a581abe207a5aa9926d2a43
kinza-rashid <127532906+kinza-rashid@users.noreply.github.com>
2023-06-16 19:34:26 +0500

    7_👨‍🦲_Dalai_Lama.py

--- pages/7_👨‍🦲_Dalai_Lama.py
+++ pages/7_👨‍🦲_Dalai_Lama.py
@@ -75,4 +75,4 @@ else:
                 output = query({
                     "inputs": prompt,
                 })
-            st.write("", output)
+            st.write("", output.generated_text)
