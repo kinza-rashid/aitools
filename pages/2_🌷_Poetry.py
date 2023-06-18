4788a0af23b864fc58a0670c4d3121bb301b74e1
kinza-rashid <127532906+kinza-rashid@users.noreply.github.com>
2023-06-18 13:47:02 +0500

    2_🌷_Poetry.py

--- pages/2_🌷_Poetry.py
+++ pages/2_🌷_Poetry.py
@@ -66,7 +66,7 @@ else:
                     0.1,
                     65,
                 ]}).json()
-            mytext = response["data"]
+            mytext = response
             
 
         c1, c2 = st.columns((1, 1))
